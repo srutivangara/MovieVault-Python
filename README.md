0d6467bf0942b76eacc32fb456765cd1a6b2af31
Sruti Vangara <vangarasruti@gmail.com>
2026-07-17 22:13:40 +0530

    Added Streamlit page configuration

---
+++ README.md
@@ -0,0 +1,4 @@
+# MovieVault
+An OTT Watchlist Manager built using Python and Streamlit.
+
+Currently under development
---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MovieVault",
+    page_icon="🎬",
+    layout="wide"
+)
+
+st.title("MovieVault")
+st.write("Welcome to my OTT watchlist Manager!")
